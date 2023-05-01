c35e9756fe665d9ed73ee07f52b4e9429360d6d8
Raniaabdullahh <raniaabdullah6600@gmail.com>
2023-05-01 15:04:05 +0300

    adding demo_01

---
+++ class-01/demo/snakes_cafe.py
@@ -0,0 +1,5 @@
+import snakes_cafe_intro #module
+# from  snakes_cafe_intro import intro #module
+
+
+# intro()
---
+++ class-01/demo/snakes_cafe_intro.py
@@ -0,0 +1,64 @@
+
+# print("**************************************")
+# print("**    Welcome to the Snakes Cafe!   **")
+# print("**    Please see our menu below.    **")
+# print("**")
+# print("** To quit at any time, type \"quit\" **")
+# print("**************************************")
+
+# refactore it & keep it DRY
+Appetizers = ["Wings",'Cookies'," Spring Rolls"]
+
+
+def intro():
+    print('''
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+''')
+
+def user_insertion():
+    user_input=input(">")  
+    return user_input        
+
+def main():
+    user_input = user_insertion()
+    while(user_input.lower() != "quit"):
+   
+        # if user_input.lower() == "quit":
+        #     end_application()        
+            if user_input in Appetizers:
+                print(user_input)
+                #TODO: handle the order numbers
+               
+            else:
+                print("sorry we dont have this item !")
+                
+            user_input = user_insertion()    
+
+    end_application()
+
+
+def end_application():
+    print("thanks for using snakes cafe application !")          
+
+#invoke the function 
+if __name__=="__main__": 
+    intro()  
+    main()
+                 
+
+
+
+
+
+
+
+
+
+
+
